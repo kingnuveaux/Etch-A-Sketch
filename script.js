449d0a88fd7cb3f394e8b16e9b5c8555db30f0a8
kingnuveaux <terrancemoore23@gmail.com>
2022-03-22 19:05:28 -0400

    Reworked code

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-const container = document.getElementById("container");
+const container = document.querySelector(".container");
+const gridSquare = document.getElementsByClassName("gridSquare");
+const clearBTN = document.querySelector(".clearBTN");
+let squareAmount = 16;
+let userAmount;
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-//Function takes user input and applies it to makeRows function
-function returnText(){
-  let input = document.getElementById("userInput").value;
-  makeRows(input, input)
+
+/* This function creates 16x16 grid or what ever input the user has given, the event listner at the
+ bottom of the function makes it that when a user hovers over a grid square it turns black. 
+*/
+function fullGrid(e) {
+  for (let i = 0; i < e*e; i++) {
+    square = document.createElement("div");
+    container.appendChild(square);
+    square.className = "gridSquare";
+    
+  }
+  for (let i = 0; i < gridSquare.length; i++) {
+    gridSquare[i].addEventListener("mouseover", () => {
+      gridSquare[i].className += " squareBlack";
+    });
+  }
+  container.style.setProperty('grid-template-columns', 'repeat(' + userAmount + ', auto)');
 }
 
-// function that clears the grid
-function clearGrid(){
-    (".grid-item").remove();
-};  
+/* This event lister the the clear button clears all drawing on the grid and asks the user how big the grid
+ that they want should be. If user enters a number below 4 or above 100 it sends another prompt asking
+ the user to select a number between 4 and 100. This also removes the original grid and replaces it
+ with the users desired size.
+*/
+clearBTN.addEventListener("click", () => {
+  for (let i = 0; i < gridSquare.length; i++) {
+    gridSquare[i].classList.remove("squareBlack");
+  }
+  userAmount = prompt(
+    "The current grid is 16x16, what number of square x square would you like?"
+  );
+  if (userAmount == "") {
+    userAmount = 16;
+  }
+  while (parseInt(userAmount) < 4 || parseInt(userAmount) > 100) {
+    userAmount = prompt(
+      `Please enter a number between 4 and 100.
+The current grid is 16x16, what number of square x square would you like?`
+    );
+  }
+  squareAmount = parseInt(userAmount);
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  fullGrid(squareAmount);
+});
 
-
-function newGrid(){
-  clearGrid()
-  let input = prompt("How many rows/columns?")
-  makeRows(input, input)
-
-}
+fullGrid(squareAmount);
 
